0f8e331f6a234e8ff1d0502e13730bf895c8049a
Dream4ever <cnhwvista@gmail.com>
2023-12-21 20:17:26 +0800

    fix: 修正 tailwind 颜色扩展代码

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,20 +3,20 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   darkMode: 'class',
   theme: {
-    colors: {
-      'black-1': '#1E1E2D',
-      'navig1': '#CFCFCF',
-      'second-gray': '#727272',
-      'black-2': '#262626',
-      'third-gray': '#858585',
-      'blue-1': '#0075FF',
-      'grdb': '#FF2DF7',
-      'grdm': '#5200FF',
-      'grde': '#00F0FF',
-    },
     extend: {
       width: {
         'content': '1280px',
+      },
+      colors: {
+        'the-black1': '#1E1E2D',
+        'navig1': '#CFCFCF',
+        'second-gray': '#727272',
+        'the-black2': '#262626',
+        'third-gray': '#858585',
+        'the-blue1': '#0075FF',
+        'grdb': '#FF2DF7',
+        'grdm': '#5200FF',
+        'grde': '#00F0FF',
       },
     },
   },
