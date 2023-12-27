97fe5d169948182080d4c062629ee47bd3e7b016
Dream4ever <cnhwvista@gmail.com>
2023-12-27 16:27:54 +0800

    feat: 添加切换颜色模式的按钮

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,23 @@
           <span>{{ link }}</span>
           <img class="w-6 h-6 dark:brightness-0 dark:invert" src="~/assets/svg/down-select.svg" alt="select">
         </a>
+        <div class="flex flex-col justify-center ml-3" @click="toggleColorMode">
+          <input type="checkbox" name="light-switch" class="light-switch sr-only" />
+          <label class="relative cursor-pointer p-2" for="light-switch">
+            <svg class="dark:hidden" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+              <path class="fill-black-1"
+                d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z" />
+              <path class="fill-black-2" d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z" />
+            </svg>
+            <svg class="hidden dark:block" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+              <path class="fill-slate-400"
+                d="M6.2 1C3.2 1.8 1 4.6 1 7.9 1 11.8 4.2 15 8.1 15c3.3 0 6-2.2 6.9-5.2C9.7 11.2 4.8 6.3 6.2 1Z" />
+              <path class="fill-slate-500"
+                d="M12.5 5a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25.625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25.625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 5Z" />
+            </svg>
+            <span class="sr-only">Switch to light / dark version</span>
+          </label>
+        </div>
       </div>
     </div>
     <div class="slogon mt-12 flex justify-between items-center">
@@ -113,4 +130,15 @@ const footerTexts = [
   'Copyright © 2023 MOVESPACE. All rights reserved.',
   'Terms of Use & Privacy Policy',
 ]
+
+const colorMode = useColorMode()
+const toggleColorMode = () => {
+  if (colorMode.preference === 'dark') {
+    colorMode.preference = 'light'
+  } else {
+    colorMode.preference = 'dark'
+  }
+}
 </script>
+
+<style scoped></style>
